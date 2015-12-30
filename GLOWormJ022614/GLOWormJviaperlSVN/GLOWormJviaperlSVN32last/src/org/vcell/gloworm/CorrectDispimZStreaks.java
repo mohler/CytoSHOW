43876714367c02ae93c5ea500946e4f8c0d812b9
wmohler <wmohler@d59c7341-666b-7e48-8bb3-2014e8cfb900>
2015-12-30 12:57:39 +0000

    git-svn-id: file:///Users/wmohler/Documents/CytoSHOWsvnClone/svn_load_compat@18896 d59c7341-666b-7e48-8bb3-2014e8cfb900

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/CorrectDispimZStreaks.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/CorrectDispimZStreaks.java
@@ -30,7 +30,7 @@ public class CorrectDispimZStreaks implements PlugIn {
 	public void run(String arg) {
 		imp = IJ.getImage();
 		int slice = imp.getSlice();
-		ImagePlus gaussianDiffImp = (new ImagePlus("http://fsbill.cam.uchc.edu/Xwords/z-x_Mask_ver_-32bkg_x255over408_1x33rect.tif"));
+		ImagePlus gaussianDiffImp = (new ImagePlus("http://fsbill.cam.uchc.edu/Xwords/z-x_Mask_ver_-32bkg_x255over408_15x33rect.tif"));
 		gaussianDiffImp.getProcessor().setMinAndMax(0, 255);
 		WindowManager.setTempCurrentImage(gaussianDiffImp);
 		if (imp.getBitDepth() == 8)
@@ -115,11 +115,11 @@ public class CorrectDispimZStreaks implements PlugIn {
 						maxCum.add(maxXs[n]+","+maxYs[n]);
 						ImageProcessor modIP = gaussianDiffIP.duplicate();
 						modIP.multiply(maskScaleFactor * (((double)imp.getProcessor().getPixel(maxXs[n], maxYs[n]))-bkgdMode)/1000);
-						imp.getProcessor().copyBits(modIP, maxXs[n], maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
-						targetIP.copyBits(modIP, maxXs[n], maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
+						imp.getProcessor().copyBits(modIP, maxXs[n]-gaussianDiffIP.getWidth()/2, maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
+						targetIP.copyBits(modIP, maxXs[n]-gaussianDiffIP.getWidth()/2, maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
 						Color fgc = Toolbar.getForegroundColor();
 						Toolbar.setForegroundColor(Color.BLACK);
-						targetIP.set(maxXs[n], maxYs[n], 0);
+						targetIP.fillOval(maxXs[n]-1, maxYs[n]-1, 3, 3);
 						Toolbar.setForegroundColor(fgc);
 
 					}
@@ -141,11 +141,11 @@ public class CorrectDispimZStreaks implements PlugIn {
 						maxCum.add(maxXs[n]+","+maxYs[n]);
 						ImageProcessor modIP = gaussianDiffIP.duplicate();
 						modIP.multiply(maskScaleFactor * (((double)imp.getProcessor().getPixel(maxXs[n], maxYs[n]))-bkgdMode)/1000);
-						imp.getProcessor().copyBits(modIP, maxXs[n], maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
-						targetIP.copyBits(modIP, maxXs[n], maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
+						imp.getProcessor().copyBits(modIP, maxXs[n]-gaussianDiffIP.getWidth()/2, maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
+						targetIP.copyBits(modIP, maxXs[n]-gaussianDiffIP.getWidth()/2, maxYs[n]-gaussianDiffIP.getHeight()/2, Blitter.DIFFERENCE);
 						Color fgc = Toolbar.getForegroundColor();
 						Toolbar.setForegroundColor(Color.BLACK);
-						targetIP.set(maxXs[n], maxYs[n], 0);
+						targetIP.fillOval(maxXs[n]-1, maxYs[n]-1, 3, 3);
 						Toolbar.setForegroundColor(fgc);
 
 					}
