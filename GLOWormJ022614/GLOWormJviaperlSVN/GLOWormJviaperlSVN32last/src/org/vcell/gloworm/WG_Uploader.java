8ad31d8b5d65953428c6928f8df75dc93781362e
wmohler <wmohler@d59c7341-666b-7e48-8bb3-2014e8cfb900>
2016-03-04 23:12:28 +0000

    work on upload plugin
    git-svn-id: file:///Users/wmohler/Documents/CytoSHOWsvnClone/svn_load_compat@19280 d59c7341-666b-7e48-8bb3-2014e8cfb900

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/WG_Uploader.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/WG_Uploader.java
@@ -8,6 +8,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
@@ -22,8 +25,26 @@ public class WG_Uploader implements PlugIn {
 		
 	}
 	
-	public WG_Uploader(String path) {
-
+	public WG_Uploader(String masterPath) {
+		ArrayList<String> iterativeDirPaths = new ArrayList<String>();
+		iterativeDirPaths.add(masterPath);
+		int increment = 0;
+		int alSize = iterativeDirPaths.size();
+		while (increment < alSize) {
+			for (int i=increment;i<alSize;i++) {
+				String iPath = iterativeDirPaths.get(i);
+				String[] localDirFileNames = (new File(iPath)).list();
+				increment++;
+				for (String fileName:localDirFileNames) {
+					if ((new File(iPath+fileName)).isDirectory()) {
+						if (!iterativeDirPaths.contains(iPath+fileName+File.separator))
+							iterativeDirPaths.add(iPath+fileName+File.separator);
+					}
+				}
+			}
+			alSize = iterativeDirPaths.size();
+		}
+		
 		FTPClient ftpc = new FTPClient();
 		try {
 			ftpc.connect("155.37.255.65");
@@ -34,20 +55,48 @@ public class WG_Uploader implements PlugIn {
 				IJ.log("FTP server refused connection.");
 			} else {
 				ftpc.login("glowormguest", "GLOWorm");
-				String[] pathChunks = path.split(""+File.separator);
-				for (String chunk:pathChunks) {
-					ftpc.makeDirectory(chunk);
-					ftpc.changeWorkingDirectory(chunk);
-				}
-				String[] saveDirFileNames = (new File(path)).list();
-				for (String fileName:saveDirFileNames) {
-					if (fileName.matches(".*")) {
-						FileInputStream fis = new FileInputStream(path +File.separator +fileName);
-						ftpc.setFileType(FTPClient.BINARY_FILE_TYPE);
-						ftpc.enterLocalPassiveMode();
-						ftpc.storeFile(fileName+".tmp", fis);
-						fis.close();
-						ftpc.rename(fileName+".tmp", fileName);
+				ftpc.makeDirectory(ftpc.getLocalAddress().toString());
+				ftpc.changeWorkingDirectory(ftpc.getLocalAddress().toString());
+				for (String path:iterativeDirPaths) {
+					String[] pathChunks = path.split(""+File.separator);
+					for (String chunk:pathChunks) {
+						ftpc.makeDirectory(chunk);
+						ftpc.changeWorkingDirectory(chunk);
+					}
+					String[] localDirFileNames = (new File(path)).list();
+					String[] remoteFileNames = ftpc.listNames();
+					for (String fileName:localDirFileNames) {
+						boolean alreadyDone= false;
+						if (remoteFileNames != null) {
+							for (String remoteFileName:remoteFileNames) {
+								if (fileName.equals(remoteFileName)) {
+									alreadyDone = true;
+									break;
+								}
+							}
+						}
+						File file = new File(path +File.separator +fileName);
+						Date fd = new Date(file.lastModified());
+						String dateTouchString = 20
+													+ IJ.pad(fd.getYear()-100, 2) 
+													+ IJ.pad(fd.getMonth()+1, 2)
+													+ IJ.pad(fd.getDate(), 2)
+													+ IJ.pad(fd.getHours(), 2)
+													+ IJ.pad(fd.getMinutes(), 2)
+													+ "."
+													+ IJ.pad(fd.getSeconds(), 2);
+						if (!file.isDirectory() && !alreadyDone) {
+							FileInputStream fis = new FileInputStream(path +File.separator +fileName);
+							ftpc.setFileType(FTPClient.BINARY_FILE_TYPE);
+							ftpc.enterLocalPassiveMode();
+							ftpc.storeFile(fileName+"_" + dateTouchString+".tmp", fis);
+							
+							fis.close();
+							ftpc.rename(fileName+"_" + dateTouchString+".tmp", fileName+"_" + dateTouchString);
+						}
+					}
+					for (int c=1;c<pathChunks.length;c++) {
+						ftpc.changeToParentDirectory();
 					}
 				}
 				ftpc.logout();
