ca0d4c24121016931326953c000566802e016839
Administrator <Administrator@PenguinWAM>
2021-06-15 06:35:06 -0400

    More work on fast patch rendering. Still not accurate in reassigning XYZ positions of objs after rapid cropped rendering...need coffee and sleep.

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/ObjEditor.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/ObjEditor.java
@@ -10,24 +10,55 @@ public class ObjEditor implements PlugIn {
 
 	@Override
 	public void run(String arg) {
-		GenericDialog gd = new GenericDialog("Specify scaling and translations for objs");
-		gd.addNumericField("scaleX", 1.0, 0, 0, "");
-		gd.addNumericField("scaleY", 1.0, 0, 0, "");
-		gd.addNumericField("scaleZ", 1.0, 0, 0, "");
-		gd.addNumericField("shiftX", 0.0, 0, 0, "");
-		gd.addNumericField("shiftY", 0.0, 0, 0, "");
-		gd.addNumericField("shiftZ", 0.0, 0, 0, "");
-		gd.showDialog();
-		double scaleX = gd.getNextNumber();
-		double scaleY = gd.getNextNumber();
-		double scaleZ = gd.getNextNumber();
-		double shiftX = gd.getNextNumber();
-		double shiftY = gd.getNextNumber();
-		double shiftZ = gd.getNextNumber();
+		String[] args = arg.split("\\|");
+		double[] scalesAndShifts = new double[args.length];
+		String objDir = "";
+		String outDir = "";
+		boolean autofilled = false;
+		for (int a=0; a<args.length; a++) {
+			if (a<args.length-2) {
+				scalesAndShifts[a] = Double.parseDouble(args[a]);
+			} else if (a==args.length-2) {
+				objDir = args[a];
+			} else if (a==args.length-1) {
+				outDir = args[a];
+				autofilled = true;
+			}
+		}
+		double scaleX = scalesAndShifts[0];
+		double scaleY = scalesAndShifts[1];
+		double scaleZ = scalesAndShifts[2];
+		double shiftX = scalesAndShifts[3];
+		double shiftY = scalesAndShifts[4];
+		double shiftZ = scalesAndShifts[5];
+		if (!autofilled) {
+			GenericDialog gd = new GenericDialog("Specify scaling and translations for objs");
+			gd.addNumericField("scaleX", 1.0, 0, 0, "");
+			gd.addNumericField("scaleY", 1.0, 0, 0, "");
+			gd.addNumericField("scaleZ", 1.0, 0, 0, "");
+			gd.addNumericField("shiftX", 0.0, 0, 0, "");
+			gd.addNumericField("shiftY", 0.0, 0, 0, "");
+			gd.addNumericField("shiftZ", 0.0, 0, 0, "");
+			gd.showDialog();
+			 scaleX = gd.getNextNumber();
+			 scaleY = gd.getNextNumber();
+			 scaleZ = gd.getNextNumber();
+			 shiftX = gd.getNextNumber();
+			 shiftY = gd.getNextNumber();
+			 shiftZ = gd.getNextNumber();
+
+			objDir = IJ.getDirectory("which file or folder of objs to edit?");
+			outDir = IJ.getDirectory("Which folder to save edited objs?");
+		}
 		
-		String objDir = IJ.getDirectory("which folder of objs to edit?");
-		String outDir = IJ.getDirectory("Which folder to save edited objs?");
-		String[] objDirList = new File(objDir).list();
+		String[] objDirList = null;
+		if (new File (objDir).isDirectory()) {
+			objDirList = new File(objDir).list();
+		} else {
+			objDirList = new String[]{new File(objDir).getName()};
+			objDir = new File(objDir).getParent();
+		}
+
 		for (String objName:objDirList) {
 			if (objName.toLowerCase().endsWith(".obj")) {
 				String[] objLines = IJ.openAsString(objDir+File.separator+objName).split("\n");
@@ -53,3 +84,4 @@ public class ObjEditor implements PlugIn {
 	}
 
 }
+
