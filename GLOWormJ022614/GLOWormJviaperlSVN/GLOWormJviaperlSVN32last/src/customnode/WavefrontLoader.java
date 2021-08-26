a6acf36644608414415d9836755fd8a21d31977e
Administrator <Administrator@PenguinWAM>
2021-08-26 07:51:17 -0400

    more twiddling fits

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/customnode/WavefrontLoader.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/customnode/WavefrontLoader.java
@@ -172,6 +172,16 @@ public class WavefrontLoader {
 //		w*(Float.parseFloat(sp[2]) - y)/(x-4736),
 //		w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736)));
 
+////		FOR MEI ADULT rot corr xz
+//		float x = 10800.0f;
+//		float y = 10816.0f;
+//		float z =  -8844.971f;
+//		float w = 50000;
+//		vertices.add(new Point3f(
+//				(float)(Math.cos(-0.227)*(w*(Float.parseFloat(sp[3]) -z)/(x-4736)) -  Math.sin(-0.227)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736))),
+//				w*(Float.parseFloat(sp[2]) - y)/(x-4736),
+//				(float)(Math.sin(-0.227)*(w*(Float.parseFloat(sp[3]) -z)/(x-4736)) +  Math.cos(-0.227)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736)))));
+
 ////		FOR L3
 //		float x = 13793f;
 //		float y = 13302f;
@@ -193,14 +203,14 @@ public class WavefrontLoader {
 //			w*(Float.parseFloat(sp[3]) -z)/(x-2804)));
 
 ////	FOR L1_3
-	float x = 8316f;
-	float y = 6042f;
-	float z = -2470f;
-	float w = 50000;
-	vertices.add(new Point3f(
-		-w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-4412),
-		w*(Float.parseFloat(sp[2]) - y)/(x-4412),
-		w*(Float.parseFloat(sp[3]) -z)/(x-4412)));
+//	float x = 8316f;
+//	float y = 6042f;
+//	float z = -2470f;
+//	float w = 50000;
+//	vertices.add(new Point3f(
+//		-w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-4412),
+//		w*(Float.parseFloat(sp[2]) - y)/(x-4412),
+//		w*(Float.parseFloat(sp[3]) -z)/(x-4412)));
 
 ////	FOR L1_2
 //	float x = 12564f;
@@ -212,15 +222,15 @@ public class WavefrontLoader {
 //	w*(Float.parseFloat(sp[2]) - y)/(x-8788),
 //	-w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788)));
 
-////	FOR L1_2 fixing rot around Yaxis
-//	float x = 12564f;
-//	float y = 7782f;
-//	float z =  -3865f;
-//	float w = 50000;
-//	vertices.add(new Point3f(
-//	-(float)(Math.cos(-0.384)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) - Math.sin(-0.384)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788))),
-//	w*(Float.parseFloat(sp[2]) - y)/(x-8788),
-//	-(float)(Math.sin(-0.384)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) + Math.cos(-0.384)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788)))));
+//	FOR L1_2 fixing rot around Yaxis
+	float x = 12564f;
+	float y = 7782f;
+	float z =  -3865f;
+	float w = 50000;
+	vertices.add(new Point3f(
+	-(float)(Math.cos(-0.495)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) - Math.sin(-0.495)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788))),
+	w*(Float.parseFloat(sp[2]) - y)/(x-8788),
+	-(float)(Math.sin(-0.495)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) + Math.cos(-0.495)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788)))));
 
 ////	FOR L1_4
 //	float x = 8402f;
