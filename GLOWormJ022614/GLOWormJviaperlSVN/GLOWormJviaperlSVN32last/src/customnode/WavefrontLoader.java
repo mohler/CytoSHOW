6f7a4667a28a6590ac1e7a121524a48ab2f1f7a7
Administrator <Administrator@PenguinWAM>
2021-11-14 04:37:11 -0500

    reactivated dataset specific rotation corrections for All Nine Brains

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/customnode/WavefrontLoader.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/customnode/WavefrontLoader.java
@@ -147,12 +147,145 @@ public class WavefrontLoader {
 	private void readVertex(boolean flipXcoords) {
 		String[] sp = line.split("\\s+");
 		int flipXCoef = flipXcoords?1:1;
+		String objFileGrandParentName = new File(objfile).getParentFile().getParentFile().getName();
+		if (new File(objfile).getParentFile().getParentFile().getParentFile().getName().contains("AllNineBrains_")) {
+			//		FOR JSH
+			if (objFileGrandParentName.contains("JSH")) {
+				float x = 20803.52f;
+				float y = 12784.513f;
+				float z = -4789.9414f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-14701),
+						w*(Float.parseFloat(sp[2]) - y)/(x-14701),
+						w*(Float.parseFloat(sp[3]) -z)/(x-14701)));
+			}
+			//		//		FOR MEI ADULT
+			//		if (objFileGrandParentName.contains("MeiAdult")) {
+			//		float x = 10800.0f;
+			//		float y = 10816.0f;
+			//		float z =  -8844.971f;
+			//		float w = 50000;
+			//		vertices.add(new Point3f(
+			//				w*(Float.parseFloat(sp[3]) -z)/(x-4736),
+			//				w*(Float.parseFloat(sp[2]) - y)/(x-4736),
+			//				w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736)));
+			//		}
+			//		FOR MEI ADULT rot corr xz
+			if (objFileGrandParentName.contains("MeiAdult")) {
+				float x = 10800.0f;
+				float y = 10816.0f;
+				float z =  -8844.971f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						(float)(Math.cos(-0.227)*(w*(Float.parseFloat(sp[3]) -z)/(x-4736)) -  Math.sin(-0.227)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736))),
+						w*(Float.parseFloat(sp[2]) - y)/(x-4736),
+						(float)(Math.sin(-0.227)*(w*(Float.parseFloat(sp[3]) -z)/(x-4736)) +  Math.cos(-0.227)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+4736)))));
+			}
+			//		FOR L3
+			if (objFileGrandParentName.contains("L3")) {
+				float x = 13793f;
+				float y = 13302f;
+				float z = -4242f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(-x+8829),
+						w*(Float.parseFloat(sp[2]) - y)/(x-8829),
+						w*(Float.parseFloat(sp[3]) -z)/(x-8829)));
+			}
+			//		FOR L2_2
+			if (objFileGrandParentName.contains("L2_2")) {
+				float x = 8002f;
+				float y = 5648f;
+				float z = -4090f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-2804),
+						w*(Float.parseFloat(sp[2]) - y)/(x-2804),
+						w*(Float.parseFloat(sp[3]) -z)/(x-2804)));
+			}
+			//	FOR L1_3
+			if (objFileGrandParentName.contains("L1_3")) {
+				float x = 8316f;
+				float y = 6042f;
+				float z = -2470f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						-w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-4412),
+						w*(Float.parseFloat(sp[2]) - y)/(x-4412),
+						w*(Float.parseFloat(sp[3]) -z)/(x-4412)));
+			}
+			//	FOR L1_2
+			//		if (objFileGrandParentName.contains("L1_2")) {
+			//	float x = 12564f;
+			//	float y = 7782f;
+			//	float z =  -3865f;
+			//	float w = 50000;
+			//	vertices.add(new Point3f(
+			//	-w*(Float.parseFloat(sp[3]) -z)/(x-8788),
+			//	w*(Float.parseFloat(sp[2]) - y)/(x-8788),
+			//	-w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788)));
+			//		}
 
-		vertices.add(new Point3f(
-				flipXCoef*Float.parseFloat(sp[1]),
-				Float.parseFloat(sp[2]),
-				Float.parseFloat(sp[3])));
-		
+			//	FOR L1_2 fixing rot around Yaxis
+			if (objFileGrandParentName.contains("L1_2")) {
+				float x = 12564f;
+				float y = 7782f;
+				float z =  -3865f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						-(float)(Math.cos(-0.495)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) - Math.sin(-0.495)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788))),
+						w*(Float.parseFloat(sp[2]) - y)/(x-8788),
+						-(float)(Math.sin(-0.495)*(w*(Float.parseFloat(sp[3]) -z)/(x-8788)) + Math.cos(-0.495)*(w*(flipXCoef*(Float.parseFloat(sp[1]) - x))/(x-8788)))));
+			}
+			//	//	FOR L1_4
+			//		if (objFileGrandParentName.contains("L1_4")) {
+			//			float x = 8402f;
+			//		float y = 12372f;
+			//		float z =  -2725f;
+			//		float w = 50000;
+			//		vertices.add(new Point3f(
+			//				w*(Float.parseFloat(sp[1]) - x)/(x-4464),
+			//				w*(Float.parseFloat(sp[2]) - y)/(x-4464),
+			//				w*(flipXCoef*(Float.parseFloat(sp[3]) - z))/(x-4464)));
+			//		}
+
+			//		FOR L1_4 with yz rotation around x axis
+			if (objFileGrandParentName.contains("L1_4")) {		
+				float x = 8402f;
+				float y = 12372f;
+				float z =  -2725f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						w*(Float.parseFloat(sp[1]) - x)/(x-4464),
+						(float)(Math.cos(-0.558)*(w*(Float.parseFloat(sp[2]) - y)/(x-4464)) - Math.sin(-0.558)*(w*(flipXCoef*(Float.parseFloat(sp[3]) - z))/(x-4464))),
+						(float)(Math.sin(-0.558)*(w*(Float.parseFloat(sp[2]) - y)/(x-4464)) + Math.cos(-0.558)*(w*(flipXCoef*(Float.parseFloat(sp[3]) - z))/(x-4464)))));
+			}
+
+			//	FOR L1_5
+			if (objFileGrandParentName.contains("L1_5")) {		
+				float x = 7990f;
+				float y = 6999f;
+				float z =  -3317f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						-(float)(Math.cos(-2.30)*(w*(Float.parseFloat(sp[1]) - x)/(x-3973)) - Math.sin(-2.30)*((w*(Float.parseFloat(sp[2]) - y)/(x-3973)))),
+						(float)(Math.sin(-2.30)*(w*(Float.parseFloat(sp[1]) - x)/(x-3973)) + Math.cos(-2.30)*((w*(Float.parseFloat(sp[2]) - y)/(x-3973)))),
+						w*(flipXCoef*(Float.parseFloat(sp[3]) - z))/(x-3973)));
+				//
+			}
+			//	FOR N2Uadult
+			if (objFileGrandParentName.contains("N2U")) {
+				float x = 19953f;
+				float y = 15255f;
+				float z =  -6105f;
+				float w = 50000;
+				vertices.add(new Point3f(
+						(float)(Math.cos(-0.384)*(w*(Float.parseFloat(sp[1]) - x)/(x-9936)) - (float)(Math.sin(-0.384)*(w*(Float.parseFloat(sp[2]) - y)/(x-9936)))),
+						(float)(Math.sin(-0.384)*(w*(Float.parseFloat(sp[1]) - x)/(x-9936)) + (float)(Math.cos(-0.384)*(w*(Float.parseFloat(sp[2]) - y)/(x-9936)))),
+						w*(flipXCoef*(Float.parseFloat(sp[3]) - z))/(x-9936)));
+			}
+		}
 	}
 
 	private HashMap<String, Color4f> readMaterials(
