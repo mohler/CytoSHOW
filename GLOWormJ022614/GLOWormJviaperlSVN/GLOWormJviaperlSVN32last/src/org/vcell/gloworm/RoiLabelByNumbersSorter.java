6acabb5dae0a9823a20b0bf112ff61892968fedc
wmohler <wmohler@cytoshow.cam.uchc.edu>
2022-08-01 23:12:51 -0400

    fixed sorting by selected chunk.

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/RoiLabelByNumbersSorter.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/RoiLabelByNumbersSorter.java
@@ -14,54 +14,47 @@ public class RoiLabelByNumbersSorter {
 	}
 	
 	static void sort(String[] labels, int from, int to, int mode) {
-		if (mode == 0) {
-			Arrays.sort(labels);
-			return;
-		}
+//		if (mode == 0) {
+//			Arrays.sort(labels);
+//			return;
+//		}
 		int listLength = labels.length;
-		boolean allSameLength = true;
-		int len0 = labels[0].length();
-		for (int i=0; i<listLength; i++) {
-			if (labels[i].length()!=len0) {
-				allSameLength = false;
-				break;
-			}
-		}
-		if (allSameLength)
-			{ij.util.StringSorter.sort(labels); return;}
 		int maxDigits = 15;		
-		String[] labels2 = null;	
-		char ch;	
+		int lDCChunksLength = 0;
+
 		for (int i=0; i<listLength; i++) {
-			//Replace needed here before split to handle the quote-quote issue without exceptions 
 			int len =0;
-			if (labels[i].contains("Nuc")) {
-				IJ.wait(0);
-			}
 			String labelDollared = labels[i].replace("\'", "$");
 			String[] labelDollaredChunks = labelDollared.split("_");
-			int lDCChunksLength = labelDollaredChunks.length;
-			try {
-				len = labelDollaredChunks[/* lDCChunksLength - 5 + */ mode].split("-")[0].length();}
-			catch (java.lang.ArrayIndexOutOfBoundsException e) {continue;}
-			String num = "";
-			for (int j=0; j<len; j++) {
-				//Replace needed here before split to handle the quote-quote issue without exceptions 
-				ch = labelDollaredChunks[/* lDCChunksLength - 5 */ + mode].split("-")[0].charAt(j);
-				if (ch>=48 && ch<=57) num += ch;
+			lDCChunksLength = labelDollaredChunks.length;
+			String[] nums = new String[lDCChunksLength];
+			nums[lDCChunksLength-1] =  labelDollaredChunks[mode];
+			int count = 0;
+			for (int ldc=lDCChunksLength-1; ldc>=0; ldc--) {
+				if (mode!= ldc) {
+					nums[count] = labelDollaredChunks[ldc];
+					count++;
+				}
 			}
-			if (num.length()==0) num = "aaaaaa";
-			num = "000000000000000" + num; // prepend maxDigits leading zeroes
-			num = num.substring(num.length()-maxDigits);
-			labels[i] = num + labels[i];
+			
+			for (String num:nums) {
+
+				num = num.split("-")[0].replace("\"", "").replace(" ", "").toLowerCase();
+				
+				if (num.length()==0) num = "aaaaaa";
+				num = "000000000000000" + num; // prepend maxDigits leading zeroes
+				num = num.substring(num.length()-maxDigits);//trim back to maxDigits total in num
+				labels[i] = num + labels[i];
+			}
+			labels[i] = labels[i].replaceAll("(0*)(([A-Z]|[a-z]|\\#)+)(.*)", "$2$4");
 		}
 		if (labels!=null) {
 			Arrays.sort(labels);
 			for (int i=0; i<listLength; i++) {
-				labels[i] = labels[i].substring(maxDigits);
+				labels[i] = labels[i].replaceAll("(.*)(\".*\".*)","$2");
 			}
 		} else {  //why on earth use this pig of a sorter instead of Arrays.sort()?
-			ij.util.StringSorter.sort(labels);
+//			ij.util.StringSorter.sort(labels);
 		}
 	}
 			
