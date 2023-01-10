1f89abeb8f737059100e4bde927065b375643061
wmohler <wmohler@cytoshow.cam.uchc.edu>
2023-01-10 12:20:15 -0500

    Added bUnwarpJ capability and an autorun program to align a stack of N2U slices.

---
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/bUnwarpJ_record_Plugin.java
@@ -0,0 +1,35 @@
+package org.vcell.gloworm;
+
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.*;
+
+public class bUnwarpJ_record_Plugin implements PlugIn {
+
+    public void run(String arg) {
+    	
+        ImagePlus imp = WindowManager.getImage("N2U_rgb_12.89xy90z_calibEllipseCrop.tif");
+       while(true){
+	        int lastNum = imp.getCurrentSlice();
+	        IJ.run(imp, "Next Slice [>]", "");
+	        int num = imp.getCurrentSlice();
+	        if (num == lastNum)
+	        	return;
+	        ImagePlus impDupNum = new ImagePlus("",imp.getProcessor());
+	        impDupNum.setTitle(""+num);
+	        impDupNum.show();
+	        ImagePlus impRegNumLessOne = WindowManager.getImage(""+(num-1)+"r.tif");
+	        IJ.run(impDupNum, "bUnwarpJ", "source_image="+ impDupNum.getTitle()+" target_image="+ impRegNumLessOne.getTitle()+" registration=Mono image_subsample_factor=0 initial_deformation=[Very Coarse] final_deformation=Fine divergence_weight=0 curl_weight=0 landmark_weight=0 image_weight=1 consistency_weight=10 stop_threshold=0.01 verbose save_transformations save_direct_transformation=/Users/wmohler/Documents/N2U_skelROIs/n2u_bUnwarpJ_testFromCytoShow"+num+"_direct_transf.txt");
+	        ImagePlus regStack = WindowManager.getImage("Registered Source Image");
+	        ImagePlus impRegNum = new ImagePlus("",regStack.getProcessor());;
+	        impRegNum.show();
+	        IJ.saveAs(impRegNum, "Tiff", "/Users/wmohler/Documents/N2U_skelROIs/n2u_bUnwarpJ_testFromCytoShow/"+num+"r.tif");
+	        impDupNum.close();
+	        impRegNumLessOne.close();
+	        regStack.close();
+        }
+    }
+
+}
