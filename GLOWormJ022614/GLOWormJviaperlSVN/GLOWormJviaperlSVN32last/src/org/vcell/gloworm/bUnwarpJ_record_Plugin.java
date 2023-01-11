809578611a6cfbc3e03d74a4c552b58e8ca2aca5
wmohler <wmohler@cytoshow.cam.uchc.edu>
2023-01-10 21:24:07 -0500

    more changes to enable applying bUnwarpJ transforms to images and tags.

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/bUnwarpJ_record_Plugin.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/org/vcell/gloworm/bUnwarpJ_record_Plugin.java
@@ -10,7 +10,7 @@ public class bUnwarpJ_record_Plugin implements PlugIn {
 
     public void run(String arg) {
     	
-        ImagePlus imp = WindowManager.getImage("N2U_rgb_12.89xy90z_calibEllipseCrop.tif");
+       ImagePlus imp = IJ.getImage();
        while(true){
 	        int lastNum = imp.getCurrentSlice();
 	        IJ.run(imp, "Next Slice [>]", "");
@@ -19,11 +19,13 @@ public class bUnwarpJ_record_Plugin implements PlugIn {
 	        	return;
 	        ImagePlus impDupNum = new ImagePlus("",imp.getProcessor());
 	        impDupNum.setTitle(""+num);
+	        impDupNum.setCalibration(imp.getCalibration());
 	        impDupNum.show();
 	        ImagePlus impRegNumLessOne = WindowManager.getImage(""+(num-1)+"r.tif");
-	        IJ.run(impDupNum, "bUnwarpJ", "source_image="+ impDupNum.getTitle()+" target_image="+ impRegNumLessOne.getTitle()+" registration=Mono image_subsample_factor=0 initial_deformation=[Very Coarse] final_deformation=Fine divergence_weight=0 curl_weight=0 landmark_weight=0 image_weight=1 consistency_weight=10 stop_threshold=0.01 verbose save_transformations save_direct_transformation=/Users/wmohler/Documents/N2U_skelROIs/n2u_bUnwarpJ_testFromCytoShow"+num+"_direct_transf.txt");
+	        IJ.run(impDupNum, "bUnwarpJ", "source_image="+ impDupNum.getTitle()+" target_image="+ impRegNumLessOne.getTitle()+" registration=Mono image_subsample_factor=0 initial_deformation=[Very Coarse] final_deformation=Fine divergence_weight=0 curl_weight=0 landmark_weight=0 image_weight=1 consistency_weight=10 stop_threshold=0.01 verbose save_transformations save_direct_transformation=/Users/wmohler/Documents/N2U_skelROIs/n2u_bUnwarpJ_testFromCytoShow/"+num+"_direct_transf.txt");
 	        ImagePlus regStack = WindowManager.getImage("Registered Source Image");
-	        ImagePlus impRegNum = new ImagePlus("",regStack.getProcessor());;
+	        ImagePlus impRegNum = new ImagePlus(""+num+"r.tif",regStack.getProcessor());
+	        impRegNum.setCalibration(imp.getCalibration());
 	        impRegNum.show();
 	        IJ.saveAs(impRegNum, "Tiff", "/Users/wmohler/Documents/N2U_skelROIs/n2u_bUnwarpJ_testFromCytoShow/"+num+"r.tif");
 	        impDupNum.close();
