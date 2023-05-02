7c6c9b8af1ad3bfaef7537279b30866fdc080553
Administrator <Administrator@PenguinWAM>
2023-05-02 02:11:53 -0400

    Fixes to slicer's use of temp virtual stacks.

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/ij/plugin/Slicer.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/ij/plugin/Slicer.java
@@ -184,44 +184,47 @@ public class Slicer implements PlugIn, TextListener, ItemListener {
 		ImagePlus imp2 = null;
 		ImageStack stack2 = null;
 		Roi roi = imp.getRoi();
-		for (int t=1; t<=frames; t++) {
-			for (int c=1; c<=channels; c++) {
-				ImageStack tmp1Stack = new ImageStack(width, height);
-				for (int z=1; z<=slices; z++) {
-					imp.setPositionWithoutUpdate(c, z, t);
-					tmp1Stack.addSlice(null, imp.getProcessor());
+		if (false && frames == 1 && channels == 1) {
+			imp2 = imp;
+		}else {
+			for (int t=1; t<=frames; t++) {
+				for (int c=1; c<=channels; c++) {
+					ImageStack tmp1Stack = new ImageStack(width, height);
+					for (int z=1; z<=slices; z++) {
+						imp.setPositionWithoutUpdate(c, z, t);
+						tmp1Stack.addSlice(null, imp.getProcessor());
+					}
+					ImagePlus tmp1 = new ImagePlus("tmp", tmp1Stack);
+					tmp1.setCalibration(imp.getCalibration());
+					tmp1.setRoi(roi);
+					ImagePlus tmp2 = reslice(tmp1);
+					int slices2 = tmp2.getStackSize();
+					if (imp2==null) {
+						imp2 = tmp2.createHyperStack("Reslice of "+imp.getTitle(), channels, slices2, frames, tmp2.getBitDepth());
+						stack2 = imp2.getStack();
+					}
+					ImageStack tmp2Stack = tmp2.getStack();
+					for (int z=1; z<=slices2; z++) {
+						imp.setPositionWithoutUpdate(c, z, t);
+						int n2 = imp2.getStackIndex(c, z, t);
+						stack2.setPixels(tmp2Stack.getPixels(z), n2);
+					}
 				}
-				ImagePlus tmp1 = new ImagePlus("tmp", tmp1Stack);
-				tmp1.setCalibration(imp.getCalibration());
-				tmp1.setRoi(roi);
-				ImagePlus tmp2 = reslice(tmp1);
-				int slices2 = tmp2.getStackSize();
-				if (imp2==null) {
-					imp2 = tmp2.createHyperStack("Reslice of "+imp.getTitle(), channels, slices2, frames, tmp2.getBitDepth());
-					stack2 = imp2.getStack();
-				}
-				ImageStack tmp2Stack = tmp2.getStack();
-				for (int z=1; z<=slices2; z++) {
-					imp.setPositionWithoutUpdate(c, z, t);
-					int n2 = imp2.getStackIndex(c, z, t);
-					stack2.setPixels(tmp2Stack.getPixels(z), n2);
+			}
+			imp.setPosition(c1, z1, t1);
+			if (channels>1 && imp.isComposite()) {
+				imp2 = new CompositeImage(imp2, ((CompositeImage)imp).getMode());
+				((CompositeImage)imp2).copyLuts(imp);
+			}
+			if (imp.getNChannels() == imp2.getNChannels()) {
+				for (int c=1; c<=imp.getNChannels(); c++) {
+					String sliceLabel = imp.getStack().getSliceLabel(c);
+					for (int t=0; t<imp2.getNFrames(); t++) {
+						imp2.getStack().setSliceLabel(sliceLabel, t*imp2.getNChannels()+c);
+					}
 				}
 			}
 		}
-		imp.setPosition(c1, z1, t1);
-		if (channels>1 && imp.isComposite()) {
-			imp2 = new CompositeImage(imp2, ((CompositeImage)imp).getMode());
-			((CompositeImage)imp2).copyLuts(imp);
-		}
-		if (imp.getNChannels() == imp2.getNChannels()) {
-			for (int c=1; c<=imp.getNChannels(); c++) {
-				String sliceLabel = imp.getStack().getSliceLabel(c);
-				for (int t=0; t<imp2.getNFrames(); t++) {
-					imp2.getStack().setSliceLabel(sliceLabel, t*imp2.getNChannels()+c);
-				}
-			}
-		}
-
 		return imp2;
 	}
 
@@ -389,8 +392,8 @@ public class Slicer implements PlugIn, TextListener, ItemListener {
 		 IJ.resetEscape();
 		 
 		 if (outputSlices > 1) {
-			 File saveDir = new File (IJ.getDirectory("home")+"Documents"+File.separator+imp.getTitle()+"_resliced");
-
+			 File saveDir = new File (IJ.getDirectory("temp")+"Documents"+File.separator+imp.getTitle()+ new GregorianCalendar().getTimeInMillis() +"_resliced");
+			 // WITHOUT THE TIMESTAMP IN FILEPATH, THE OLD VERSION WOULD REUSE OLD TMP_RESLICE FOLDER, BAD NEWS.  WAS AN UNCLEVER WAY OF DOING RESLICE OF VOLUMES TOO SMALL TO FIT IN MEMORY, RESUMING AFTER CRASH.
 			 //		 File saveDir = new File(imp.getOriginalFileInfo().directory+File.separator+imp.getTitle()+"_resliced");
 			 saveDir.mkdirs();
 			 for (int i=0; i<outputSlices; i++)	{
