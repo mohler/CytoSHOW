a02f088d43db516ec05f9c8df2b42bc2e3e37cfe
William Mohler <mohler.william@gmail.com>
2025-10-12 21:40:51 -0400

    Final coincidence check between peaks found and maxes found via differing algorithms.

--- GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/ij/plugin/PeakDetectNormalizer.java
+++ GLOWormJ022614/GLOWormJviaperlSVN/GLOWormJviaperlSVN32last/src/ij/plugin/PeakDetectNormalizer.java
@@ -2,7 +2,12 @@ package ij.plugin;
 
 import java.awt.Polygon;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.ListModel;
 
 import ij.*;
 import ij.gui.Roi;
@@ -33,6 +38,8 @@ public class PeakDetectNormalizer implements PlugIn {
         for (int impID : impIDs) {
         	ImagePlus nextImp = WindowManager.getImage(impID);
         	RoiManager nextRM = nextImp.getRoiManager();
+    		ArrayList<Integer> coincidentHitRoiIndexes = new ArrayList<Integer>();
+
         	IJ.log(nextImp.getTitle());
         	int maxValue = 0;
         	for (int z=1; z<=nextImp.getNSlices(); z++) {
@@ -49,7 +56,8 @@ public class PeakDetectNormalizer implements PlugIn {
         	for (int z=1; z<=nextImp.getNSlices(); z++) {
         		nextImp.setPositionWithoutUpdate(nextImp.getChannel(),z,nextImp.getFrame());
         		ImageProcessor ip = nextImp.getProcessor();
-
+        		
+        		//ROUND ONE: SEGEMENT PARTICLES AT SUCCESSIVELY DECREASING THRESHOLDED INTENSITY SLICES
         		for (int i=maxValue;i>=span;i=i-span) {
 
         			ip.setThreshold(i-span+1, maxValue, ImageProcessor.RED_LUT);
@@ -59,28 +67,28 @@ public class PeakDetectNormalizer implements PlugIn {
 
         		nZspecRois[z-1] = nextImp.getRoiManager().getCount() - totalRoiCount;
         		
-        		//OVERLAP CHECKING BLOCKS --- now running once all intensity thresholds have been sampled for a given Z value.        	
+        		//ROUND TWO:  OVERLAP CHECKING BLOCKS --- now running once all intensity thresholds have been sampled for a given Z value.        	
         		ArrayList<Integer> redundantRoiIndexes = new ArrayList<Integer>();
-        		Roi[] roisArray = nextRM.getFullRoisAsArray();
-        		int roisLength = roisArray.length;
+        		Roi[] roisRdTwoArray = nextRM.getFullRoisAsArray();
+        		int roisRdTwoLength = roisRdTwoArray.length;
 
-        		for (int maxCandidtateIndex = totalRoiCount; maxCandidtateIndex<roisLength; maxCandidtateIndex++) {   //outer loop through all rois
+        		for (int threshPeakCandidtateIndex = totalRoiCount; threshPeakCandidtateIndex<roisRdTwoLength; threshPeakCandidtateIndex++) {   //outer loop through all rois
 
-        			for (int likelySubPeakIndex = totalRoiCount; likelySubPeakIndex<roisLength; likelySubPeakIndex++ ) {    //inner loop through all rois
+        			for (int likelySubPeakIndex = totalRoiCount; likelySubPeakIndex<roisRdTwoLength; likelySubPeakIndex++ ) {    //inner loop through all rois
 
-        				if ((roisArray[likelySubPeakIndex]!=(roisArray[maxCandidtateIndex]))) {    //supposed to skip identity comparisons for same roi
+        				if ((roisRdTwoArray[likelySubPeakIndex]!=(roisRdTwoArray[threshPeakCandidtateIndex]))) {    //supposed to skip identity comparisons for same roi
 
-        					for (int pt=0; pt<roisArray[likelySubPeakIndex].getPolygon().npoints; pt++) {
+        					for (int pt=0; pt<roisRdTwoArray[likelySubPeakIndex].getPolygon().npoints; pt++) {
 
-        						if (roisArray[maxCandidtateIndex].getPolygon().contains(roisArray[likelySubPeakIndex].getPolygon().xpoints[pt], 
-        								roisArray[likelySubPeakIndex].getPolygon().ypoints[pt])){
+        						if (roisRdTwoArray[threshPeakCandidtateIndex].getPolygon().contains(roisRdTwoArray[likelySubPeakIndex].getPolygon().xpoints[pt], 
+        								roisRdTwoArray[likelySubPeakIndex].getPolygon().ypoints[pt])){
 
-        							if (getArea(roisArray[likelySubPeakIndex].getPolygon()) > getArea(roisArray[maxCandidtateIndex].getPolygon())) {
+        							if (getArea(roisRdTwoArray[likelySubPeakIndex].getPolygon()) > getArea(roisRdTwoArray[threshPeakCandidtateIndex].getPolygon())) {
         								if (!redundantRoiIndexes.contains(likelySubPeakIndex))
         									redundantRoiIndexes.add(likelySubPeakIndex);
         							} else {
-        								if (!redundantRoiIndexes.contains(maxCandidtateIndex)) {
-        									redundantRoiIndexes.add(maxCandidtateIndex);
+        								if (!redundantRoiIndexes.contains(threshPeakCandidtateIndex)) {
+        									redundantRoiIndexes.add(threshPeakCandidtateIndex);
         								}
         							}  // end if getArea else
 
@@ -97,25 +105,104 @@ public class PeakDetectNormalizer implements PlugIn {
 
         		}   //outer roi loop
 
+        		ListModel<String> flm = nextRM.getFullListModel();
+        		ListModel<String> lm = nextRM.getListModel();
+
         		redundantRoiIndexes.sort(Comparator.naturalOrder());
-        		
+
+        		nextRM.getList().setModel(new DefaultListModel<String>());
         		for (int s= redundantRoiIndexes.size()-1; s>=0; s--) {
         			int rri = redundantRoiIndexes.get(s);
-        			nextRM.getFullListModel().removeElement(nextRM.getListModel().getElementAt(rri));
+
+        			((DefaultListModel<String>) flm).removeElement(nextRM.getListModel().getElementAt(rri));
         			nextRM.getROIs().remove(nextRM.getListModel().getElementAt(rri));
-        			nextRM.getListModel().remove(rri);
+        			((DefaultListModel<String>) lm).remove(rri);
+
+
         		}
-        		// END OVERLAP CHECKING/PURGE BLOCKS.
+        		nextRM.getList().setModel(lm);
+         			
+       		// END OVERLAP CHECKING/PURGE BLOCKS.
 
         		IJ.wait(0);
-        		totalRoiCount = nextImp.getRoiManager().getCount();
+        		
+        		// ROUND THREE: NOW FIND MAXIMA USING THE BUILT-IN ALGORITHM
                 ByteProcessor typeP = new ByteProcessor(nextImp.getWidth(), nextImp.getHeight());     
                 byte[] types = (byte[])typeP.getPixels();
                 MaximumFinder maxFinder = new MaximumFinder();
                 maxFinder.setup("", nextImp);
         		maxFinder.findMaxima(nextImp.getProcessor(), 30, ImageProcessor.NO_THRESHOLD, MaximumFinder.POINT_SELECTION, true, false);
+
+        		//NOW ROUND FOUR: SEEKING DOUBLE-HITS THAT ARE BOTH FIND MAXIMA POSITIVE, AND SINKING THRESHOLD POSITIVE.        	
+        		Roi[] roisRdFourArray = nextRM.getFullRoisAsArray();
+        		int roisRdFourLength = roisRdFourArray.length;
+
+        		for (int maxCandidateIndex = totalRoiCount; maxCandidateIndex<roisRdFourLength; maxCandidateIndex++) {   //outer loop through all rois
+        			if (roisRdFourArray[maxCandidateIndex].getName().contains("Traced"))
+        				continue;
+        			for (int threshCandidateIndex = totalRoiCount; threshCandidateIndex<roisRdFourLength; threshCandidateIndex++ ) {    //inner loop through all rois
+            			if (roisRdFourArray[threshCandidateIndex].getName().contains("Oval"))
+            				continue;
+            			if ((roisRdFourArray[threshCandidateIndex]!=(roisRdFourArray[maxCandidateIndex]))) {    //supposed to skip identity comparisons for same roi
+
+            				for (int ptMaxCInd=0; ptMaxCInd<roisRdFourArray[maxCandidateIndex].getPolygon().npoints; ptMaxCInd++) {
+            					for (int ptThrCInd=0; ptThrCInd<roisRdFourArray[threshCandidateIndex].getPolygon().npoints; ptThrCInd++) {
+
+
+            						if (roisRdFourArray[threshCandidateIndex].getPolygon().contains(roisRdFourArray[maxCandidateIndex].getPolygon().xpoints[ptMaxCInd], 
+            								roisRdFourArray[maxCandidateIndex].getPolygon().ypoints[ptMaxCInd])
+            								|| roisRdFourArray[maxCandidateIndex].getPolygon().contains(roisRdFourArray[threshCandidateIndex].getPolygon().xpoints[ptThrCInd], 
+            										roisRdFourArray[threshCandidateIndex].getPolygon().ypoints[ptThrCInd])){
+
+
+            							if (getArea(roisRdFourArray[maxCandidateIndex].getPolygon()) > getArea(roisRdFourArray[threshCandidateIndex].getPolygon())) {
+            								if (!coincidentHitRoiIndexes.contains(maxCandidateIndex))
+            									coincidentHitRoiIndexes.add(maxCandidateIndex);
+            							} else {
+            								if (!coincidentHitRoiIndexes.contains(maxCandidateIndex)) {
+            									coincidentHitRoiIndexes.add(maxCandidateIndex);
+            								}
+            							}  // end if reverse getArea compare else
+
+            							break;
+
+            						}  //end if overlap
+
+            					} //end for ptThr...
+            				} //end for ptMax...
+            			}else {      
+        					continue;
+        				}   // end if != else
+
+        			}   //inner roi loop
+
+        		}   //outer roi loop
+
+        		// END SEEKING DOUBLE-HITS....
+
+        		totalRoiCount = nextImp.getRoiManager().getCount();
+
+        	
         	}  // end for Z loop
 
+    		coincidentHitRoiIndexes.sort(Comparator.naturalOrder());
+    		Roi[] crossConfirmedRois = new Roi[coincidentHitRoiIndexes.size()];
+    		
+    		for (int s= coincidentHitRoiIndexes.size()-1; s>=0; s--) {
+    			int rri = coincidentHitRoiIndexes.get(s);
+    			crossConfirmedRois[s] = ((Roi)nextRM.getROIs().get(nextRM.getListModel().getElementAt(rri)).clone());
+     		}
+
+    		// recite steps involved in rm.delete() with none selected, but without worrying about internal rm flags...
+    		nextRM.getROIs().clear();
+    		nextRM.getRoisByNumbers().clear();
+    		nextRM.getRoisByRootName().clear();
+    		nextRM.getListModel().removeAllElements();
+			nextRM.getFullListModel().removeAllElements();
+    		
+    		for (Roi ccRoi:crossConfirmedRois) {
+    			nextRM.addRoi(ccRoi, false, ccRoi.getStrokeColor(), ccRoi.getFillColor(), -1, false);
+    		}
         }  // end for impID loop
 
         
